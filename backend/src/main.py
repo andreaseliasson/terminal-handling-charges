8a6e47fd0d5b2fb518b8e49ae9ed8e3fc845ceb4
Andreas Eliason <andreashg.eliasson@gmail.com>
2017-03-11 11:27:35 +0000

    Add response headers

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,8 +3,16 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def start():
     return "Let's check for anomalies in terminal handling charges"
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST')
+    return response
 
 if __name__ == "__main__":
     app.run()
