b026036fe67636cb1b013a0d042b2bd0afe07a69
Andreas Eliason <andreashg.eliasson@gmail.com>
2017-03-13 12:07:55 +0000

    wip: add logic for dealing with new charges

--- backend/src/helper.py
+++ backend/src/helper.py
@@ -1,13 +1,21 @@
 import json
 import pandas as pd
 import numpy as np
-from outlier_detection import get_outliers
+import copy
+from outlier_detection import get_outliers, percentile_based_outlier
+
+ACCUMULATED_CHARGES_OUTPUT = '../data/accumulated_data.json'
 
 
 def load_json(file_name):
     with open(file_name, 'r') as f:
         data = json.load(f)
     return data
+
+
+def dump_json(charges, file_name):
+    with open(file_name, 'w') as fp:
+        json.dump(charges, fp)
 
 
 def get_2_letter_country_codes(charges):
@@ -27,3 +35,35 @@ def get_country_charges_with_outliers(charges, country_codes):
                                             'values': values.tolist(),
                                             'outliers': list(outliers)})
     return accumulated_country_charges
+
+
+# Naive way of re-using the existing logic to check for outliers with new charge included
+# Not very efficient
+def is_outlier(sample_charges, new_charge):
+    charges_df = pd.DataFrame(sample_charges)
+
+    country_charges = charges_df[charges_df['port'].str.contains(r'^' + new_charge['port'][:2])]
+    sample_values = np.array(country_charges['value'])
+
+    # Append new charge value
+    values = np.append(sample_values, [float(new_charge['value'])])
+
+    # Check if last charge that we just appended is an outlier
+    return percentile_based_outlier(values)[-1]
+
+
+def save_new_charge(acc_charges, new_charge):
+    # Add new property indicating whether or not the new charge is an outlier or not
+    new_charge['outlier'] = int(is_outlier(acc_charges, new_charge))
+    acc_charges.append(new_charge)
+
+    # Dump list of dicts to a .json file for later references
+    dump_json(acc_charges, ACCUMULATED_CHARGES_OUTPUT)
+
+    return new_charge
+
+
+def create_acc_charges(sample_charges):
+    accumulated_charges = copy.deepcopy(sample_charges)
+    dump_json(accumulated_charges, ACCUMULATED_CHARGES_OUTPUT)
+    return accumulated_charges
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from helper import load_json, get_2_letter_country_codes, get_country_charges_with_outliers
+import json
+from helper import (load_json,
+                    get_2_letter_country_codes,
+                    get_country_charges_with_outliers,
+                    is_outlier,
+                    create_acc_charges,
+                    save_new_charge)
 from normalize_currency import (get_unique_currencies,
                                 get_currency_exchange_rates_from_api,
                                 get_currency_exchange_rates_from_file,
@@ -13,16 +19,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    charges = load_json('../data/sample_data.json')
-    currency_rates = get_currency_exchange_rates_from_file('../data/currency_rates.json')
-    normalized_charges = normalize_charges(charges, currency_rates)
-    country_charges = get_country_charges_with_outliers(normalized_charges, get_2_letter_country_codes(charges))
-
-    return jsonify(country_charges)
+    # charges = load_json('../data/sample_data.json')
+    # currency_rates = get_currency_exchange_rates_from_file('../data/currency_rates.json')
+    # normalized_charges = normalize_charges(charges, currency_rates)
+    # country_charges = get_country_charges_with_outliers(normalized_charges, get_2_letter_country_codes(charges))
+    #
+    # return jsonify(country_charges)
+    return ''
 
 
 @app.route('/charge', methods=['POST'])
 def submit_new_charge():
+    new_charge = request.json
+
     return "posting"
 
 
@@ -34,13 +43,20 @@ def after_request(response):
     return response
 
 if __name__ == "__main__":
-    app.run()
-    # charges = load_json('../data/sample_data.json')
+    # app.run()
+    charges = load_json('../data/sample_data.json')
     # unique_currencies = get_unique_currencies(charges)
     # # rates = get_currency_exchange_rates_from_api(unique_currencies)
-    # currency_rates = get_currency_exchange_rates_from_file('../data/currency_rates.json')
-    # normalized_charges = normalize_charges(charges, currency_rates)
-    # country_charges = get_country_charges_with_outliers(normalized_charges, get_2_letter_country_codes(charges))
+    currency_rates = get_currency_exchange_rates_from_file('../data/currency_rates.json')
+    normalized_charges = normalize_charges(charges, currency_rates)
+    country_charges = get_country_charges_with_outliers(normalized_charges, get_2_letter_country_codes(charges))
     #
     # # Distribution and outliers
     # # compare_outlier_methods(normalized_charges, get_2_letter_country_codes(charges))
+    new_charge = {'currency': '1', 'value': '120', 'port': 'CNSHA', 'supplier_id': '2'}
+    outlier = is_outlier(normalized_charges, new_charge)
+    print(outlier)
+    accumulated_charges = create_acc_charges(charges)
+    saved_new_charge = save_new_charge(accumulated_charges, new_charge)
+    print("saved new charge")
+    print(saved_new_charge)
