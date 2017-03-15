a4a11c1ba1fd4608180dc7aaa313e7ff1fe41eee
Andreas Eliason <andreashg.eliasson@gmail.com>
2017-03-15 00:05:56 +0000

    Normalize new charge value to USD before feeding into outlier detection

--- backend/src/charges.py
+++ backend/src/charges.py
@@ -3,6 +3,7 @@ import numpy as np
 import copy
 from outlier_detection import get_outliers, percentile_based_outlier
 from helper import dump_json
+from normalize_currency import normalize_charge
 
 ACCUMULATED_CHARGES_OUTPUT = '../data/accumulated_data.json'
 
@@ -32,13 +33,14 @@ def get_country_charges_with_outliers(charges, country_codes):
     return accumulated_country_charges
 
 
-def is_outlier(sample_charges, new_charge):
+def is_outlier(sample_charges, new_charge, currency_rates):
     """
     Naive way of checking if a new charge is an outlier by using the same algorithm we did when checking
     for the sample outliers. Based on exploring different outlier detection methods we will use the percentile
     based algorithm.
     :param sample_charges: the original sample charges
     :param new_charge: a new charge to check
+    :param currency_rates: currency rates used to normalize value
     :return: Boolean indicating whether the new charge is an outlier
     """
     charges_df = pd.DataFrame(sample_charges)
@@ -46,22 +48,27 @@ def is_outlier(sample_charges, new_charge):
     country_charges = charges_df[charges_df['port'].str.contains(r'^' + new_charge['port'][:2])]
     sample_values = np.array(country_charges['value'])
 
+    # Normalize value to USD before feeding into the outlier detection
+    normalized_new_charge = normalize_charge(new_charge, currency_rates)
+
     # Append new charge value
-    values = np.append(sample_values, [new_charge['value']])
+    values = np.append(sample_values, [normalized_new_charge['value']])
 
     # Check if last charge that we just appended is an outlier
     return percentile_based_outlier(values)[-1]
 
 
-def save_new_charge(sample_charges, acc_charges, new_charge):
+def save_new_charge(sample_charges, acc_charges, new_charge, currency_rates):
     """
     Save a new charge with a flag indicating if it's an outlier of it's ok (1 == outlier, 0 == ok).
     :param sample_charges: the original sample charges
     :param acc_charges: the accumulated charges to append the new charge to
+    :param new_charge: new charge to add
+    :param currency_rates: currency rates
     :return: new charge as a dict
     """
     # Add new property indicating whether or not the new charge is an outlier or not
-    new_charge['outlier'] = int(is_outlier(sample_charges, new_charge))
+    new_charge['outlier'] = int(is_outlier(sample_charges, new_charge, currency_rates))
     acc_charges.append(new_charge)
 
     # Dump list of dicts to a .json file for later references
